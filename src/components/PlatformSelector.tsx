8366d967ec59d599270a3ea89537ed770238d803
Shital769 <110831382+Shital769@users.noreply.github.com>
2023-04-11 18:49:06 +1000

    rafactor: extract a query object.

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -2,11 +2,14 @@ import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
 import { BsChevronDown } from "react-icons/bs";
 import usePlatforms from "../hooks/usePlatforms";
 import { Platform } from "../hooks/useGames";
+import { Genre } from "../hooks/useGenres";
 
 interface Props {
   onSelectPlatform: (platform: Platform) => void;
   selectedPlatform: Platform | null;
 }
+
+
 
 const PlatformSelector = ({ onSelectPlatform, selectedPlatform }: Props) => {
   const { data } = usePlatforms();
