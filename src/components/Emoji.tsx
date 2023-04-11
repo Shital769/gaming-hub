77130339fc883f16c6f2f87df67c978b43129007
Shital769 <110831382+Shital769@users.noreply.github.com>
2023-04-11 21:07:12 +1000

    Added some emojis.

---
+++ src/components/Emoji.tsx
@@ -0,0 +1,19 @@
+import { ImageProps, Image } from "@chakra-ui/react";
+import bullsEye from "../assets/bullsEye.webp";
+import meh from "../assets/meh.webp";
+import thumbsUp from "../assets/thumbsUp.webp";
+interface Props {
+  rating: number;
+}
+
+const Emoji = ({ rating }: Props) => {
+  if (rating < 3) return null;
+  const emojiMap: { [key: number]: ImageProps } = {
+    3: { src: meh, alt: "meh", boxSize: "25px" },
+    4: { src: thumbsUp, alt: "recommended", boxSize: "25px" },
+    5: { src: bullsEye, alt: "exceptional", boxSize: "25px" },
+  };
+  return <Image {...emojiMap[rating]} marginTop={1} />;
+};
+
+export default Emoji;
